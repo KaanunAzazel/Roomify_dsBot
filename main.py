e207ba0acc62087b003f3edcba3c3bfbe30c8ea9
KaanunAzazel <kaanun.azazel@gmail.com>
2023-12-02 12:46:59 -0300

    autocreate party and delete

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import dotenv
 import discord
@@ -13,6 +14,7 @@ def exec():
     intents = discord.Intents.default()
     intents.message_content = True 
     bot = commands.Bot(command_prefix='!', intents=intents)
+            
 
     @bot.event
     async def on_ready():
@@ -20,9 +22,10 @@ def exec():
         # task_test.start()
 
     @bot.command()
-    async def test(ctx):
+    async def createParty(ctx, nome:str):
         guild = ctx.guild
-        nome_canal = f"{ctx.author.display_name}'s group"
+        # nome_canal = f"{ctx.author.display_name}'s group"
+        nome_canal = nome
 
         existing_channel = discord.utils.get(guild.voice_channels, name=nome_canal)
         if existing_channel:
@@ -30,14 +33,23 @@ def exec():
             return
         
         categoria_desejada = discord.utils.get(guild.categories, name="︶︶︶ Party Zone ︶︶︶")
-
         if not categoria_desejada:
             await ctx.send(f"A categoria 'NOME_DA_CATEGORIA' não foi encontrada. Certifique-se de que a categoria existe.")
             return
         
         await guild.create_voice_channel(nome_canal, category=categoria_desejada)
         await ctx.send(f"{nome_canal} criado com sucesso! Entrem pra iniciarem sua jornada")
+        
+        await asyncio.sleep(9)
+        
+        while True:
+            canal_de_voz = discord.utils.get(guild.voice_channels, name=nome_canal)
+            if len(canal_de_voz.voice_states) == 0:
+                await canal_de_voz.delete()
+                break
+            await asyncio.sleep(2)
 
+        
 
 
     @tasks.loop(seconds=3)
@@ -45,7 +57,7 @@ def exec():
         channel = bot.get_channel(ID_CHANNEL_MSG)
         await channel.send('Bot inicializado')
     
-    bot.run(BOT_TOKEN   )
+    bot.run(BOT_TOKEN)
 
 
 if __name__ == "__main__":   
