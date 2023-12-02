8634bcff0bdb5dede8d94dfeff4284a5a81e9290
KaanunAzazel <kaanun.azazel@gmail.com>
2023-12-02 12:12:09 -0300

    commad create party

--- main.py
+++ main.py
@@ -12,12 +12,33 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 def exec():
     intents = discord.Intents.default()
     intents.message_content = True 
-    bot = commands.Bot(command_prefix='.', intents=intents)
+    bot = commands.Bot(command_prefix='!', intents=intents)
 
     @bot.event
     async def on_ready():
         print('Bot iniciado!')
-        task_test.start()
+        # task_test.start()
+
+    @bot.command()
+    async def test(ctx):
+        guild = ctx.guild
+        nome_canal = f"{ctx.author.display_name}'s group"
+
+        existing_channel = discord.utils.get(guild.voice_channels, name=nome_canal)
+        if existing_channel:
+            await ctx.send(f"Já existe um canal de voz com o nome {nome_canal}. Entre nele")
+            return
+        
+        categoria_desejada = discord.utils.get(guild.categories, name="︶︶︶ Party Zone ︶︶︶")
+
+        if not categoria_desejada:
+            await ctx.send(f"A categoria 'NOME_DA_CATEGORIA' não foi encontrada. Certifique-se de que a categoria existe.")
+            return
+        
+        await guild.create_voice_channel(nome_canal, category=categoria_desejada)
+        await ctx.send(f"{nome_canal} criado com sucesso! Entrem pra iniciarem sua jornada")
+
+
 
     @tasks.loop(seconds=3)
     async def task_test():
